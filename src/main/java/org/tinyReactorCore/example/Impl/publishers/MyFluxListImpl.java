af9f6e038948c5d0177bc49f003ec7189642e3c2
bensilvan <62750059+bensilvan@users.noreply.github.com>
2023-09-22 22:22:16 +0300

    make the list publisher thread safe

--- src/main/java/org/tinyReactorCore/example/Impl/publishers/MyFluxListImpl.java
+++ src/main/java/org/tinyReactorCore/example/Impl/publishers/MyFluxListImpl.java
@@ -1,24 +1,23 @@
 package org.tinyReactorCore.example.Impl.publishers;
 
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class MyFluxListImpl<T> extends MyFlux<T>{
-
-    private Integer index;
-    private final List<T> list;
+    private final ArrayBlockingQueue<T> queue;
 
     public MyFluxListImpl(List<T> list) {
-        this.list = list;
-        this.index = 0;
+        var queue = new ArrayBlockingQueue<T>(list.size());
+        queue.addAll(list);
+        this.queue = queue;
     }
     @Override
     public void onRequest(Integer count) {
-        for (var i = 0; i < count; i++) {
-            if (index < this.list.size()) {
-                super.subscriber.onNext(this.list.get(this.index++));
-            } else {
-                super.subscriber.onComplete();
-                return;
+        if (this.queue.isEmpty()) {
+            this.subscriber.onComplete();
+        } else {
+            for (var i = 0; i < count; i ++) {
+                this.subscriber.onNext(this.queue.poll());
             }
         }
     }
