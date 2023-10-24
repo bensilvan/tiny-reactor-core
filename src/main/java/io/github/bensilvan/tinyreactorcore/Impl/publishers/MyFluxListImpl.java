89c1a5cab6fea9d7f900db62d4ab35a8a49bc7fe
bensilvan <62750059+bensilvan@users.noreply.github.com>
2023-10-24 04:12:55 +0300

    make the list publisher thread safe

--- src/main/java/io/github/bensilvan/tinyreactorcore/Impl/publishers/MyFluxListImpl.java
+++ src/main/java/io/github/bensilvan/tinyreactorcore/Impl/publishers/MyFluxListImpl.java
@@ -1,23 +1,54 @@
 package io.github.bensilvan.tinyreactorcore.Impl.publishers;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class MyFluxListImpl<T> extends MyFlux<T>{
-    private final ArrayBlockingQueue<T> queue;
+public class MyFluxListImpl<T> extends MyFlux<T> {
+    private final List<T> items;
+
+    private final Integer size;
+
+    private final AtomicInteger nextIndex;
+
+    private final AtomicInteger currentIndex;
+
+    private final ConcurrentHashMap<Integer, T> itemsToSendById;
 
     public MyFluxListImpl(List<T> list) {
-        var queue = new ArrayBlockingQueue<T>(list.size());
-        queue.addAll(list);
-        this.queue = queue;
+        this.items = new ArrayList<>(list);
+        this.size = list.size();
+        this.itemsToSendById = new ConcurrentHashMap<>();
+        this.nextIndex = new AtomicInteger(0);
+        this.currentIndex = new AtomicInteger();
     }
+
     @Override
     public void onRequest(Long count) {
-        if (this.queue.isEmpty()) {
-            this.subscriber.onComplete();
-        } else {
-            for (var i = 0; i < count; i ++) {
-                this.subscriber.onNext(this.queue.poll());
+        for (var i = 0; i < count; i++) {
+            var currentIndex = this.currentIndex.getAndIncrement();
+            if (currentIndex >= this.size) {
+                this.subscriber.onComplete();
+                break;
+            }
+            this.itemsToSendById.computeIfAbsent(currentIndex, this.items::get);
+
+            while (true) {
+                var nextItem = this.itemsToSendById.remove(this.nextIndex.get());
+                if (nextItem != null) {
+                    this.subscriber.onNext(nextItem);
+                    this.itemsToSendById.remove(this.nextIndex.getAndIncrement());
+                } else {
+                    break;
+                }
             }
         }
     }
